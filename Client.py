8759ec721978f35b915ab40a03726d1f1f9d01a4
bwolf02 <bwolf2@uwo.ca>
2021-12-03 09:56:23 -0500

    Made some changes

--- Client.py
+++ Client.py
@@ -5,125 +5,128 @@ import sys
 import ipaddress
 
 
-# Continuously listens for data from the server and displays any received messages.
-# This function runs in its own thread so it can listen constantly.
-def receive():
-    while True:
-        try:
-            # Wait for new data from the server and decode it to a string
-            msg = client_socket.recv(BUFFER_SIZE).decode("utf8")
-            
-            # Add the new message to the message list
-            msg_list.insert(tkinter.END, msg)
-            
-            # Update the message list to show the new message
-            msg_list.see(tkinter.END)
-        except OSError:
-            break
-
 # Sends whatever is in the message box to the server. This function is called by the send button,
 # pressing enter in the message field, or as part of the event handler for closing the program.
-def send(event=None):
+def sendMessage(event=None):
     # Get message string from message field
-    msg = my_msg.get()
+    message = myMessage.get()
     
     # Clear message field after reading message
-    my_msg.set("")
+    myMessage.set("")
 
     # If the message is the exit command (sent as part of the event handler for closing the window),
     # then send a final message and exit the program.
-    if msg == "!exit":
+    if message == "!exit":
         # Send message "<username> has left"
-        client_socket.send(bytes(my_username.get() + " has left", "utf8"))
+        clntSocket.send(bytes(myUsername.get() + " has left", "utf8"))
         
         # Close the socket
-        client_socket.close()
+        clntSocket.close()
         
         # Close window
-        top.quit()
+        wind.quit()
         return
     
     # If execution gets here, then the message is a normal message. So, send it to the server along
     # with the user's username. (it will be broadcast to all other clients in the same room)
-    client_socket.send(bytes(my_username.get() + ": " + msg, "utf8"))
+    clntSocket.send(bytes(myUsername.get() + ": " + message, "utf8"))
 
+# Continuously listens for data from the server and displays any received messages.
+# This function runs in its own thread so it can listen constantly.
+def receiveData():
+    while True:
+        try:
+            # Wait for new data from the server and decode it to a string
+            message = clntSocket.recv(BUFFER_SIZE).decode("utf8")
+            
+            # Add the new message to the message list
+            messagelist.insert(tkinter.END, message)
+            
+            # Update the message list to show the new message
+            messagelist.see(tkinter.END)
+        except OSError:
+            break
 
-# Sends the exit command to the server. This function is called when the user clicks the X button to close the window.
-def on_closing(event=None):
-    # Set the message variable to the exit command
-    my_msg.set("!exit")
-    
-    #Send the command to the server
-    send()
 
 # Sends a command to the server to change the client's room number. Called by the Change Room button
-def change_room():
-    global current_room
+def changeRoom():
+    global currRoom
     
     # Get new room number from the selected room option
-    current_room = ((chatRoomSelected.get()).split(' '))[2]
+    currRoom = ((chatRoomSel.get()).split(' '))[2]
     
     # Send room change command to the server
-    client_socket.send(bytes("#" + current_room, "utf8"))
+    clntSocket.send(bytes("#" + currRoom, "utf8"))
     
     # Clear the message list
-    msg_list.delete(0, tkinter.END)
+    messagelist.delete(0, tkinter.END)
     
     # Show a greeting message for the new room
-    msg_list.insert(tkinter.END, "You are now in room " + str(current_room))
-    msg_list.see(tkinter.END)
+    messagelist.insert(tkinter.END, "Welcome! You are in room " + str(currRoom))
+    messagelist.see(tkinter.END)
+
+# Sends the exit command to the server. This function is called when the user clicks the X button to close the window.
+def closing(event=None):
+    # Set the message variable to the exit command
+    myMessage.set("!exit")
+    
+    #Send the command to the server
+    sendMessage()
+
+
 
 
 # ================ Begin main program ================
-number_of_rooms = 0
-current_room = 0
+currRoom = 0
+numberOfRooms = 0
+
 
 # Initialize window
-top = tkinter.Tk()
-top.title("3313 Chat App")
-top['background'] = '#505050'
-messages_frame = tkinter.Frame(top)
+wind = tkinter.Tk()
+wind.title("3313 Chat App")
+wind['background'] = '#505050'
+msgFrame = tkinter.Frame(wind)
 
 # Initialize variables to be linked to input elements
-# my_msg: used for the message entry field
-my_msg = tkinter.StringVar()
-my_msg.set("")
+# myMessage: used for the message entry field
+myMessage = tkinter.StringVar()
+myMessage.set("")
 
-# my_username: self-explanatory
-my_username = tkinter.StringVar()
-my_username.set("")
+# myUsername: self-explanatory
+myUsername = tkinter.StringVar()
+myUsername.set("")
 
 # Initialize message container, add a scrollbar, and place it in the window
-scrollbar = tkinter.Scrollbar(messages_frame, bg = "#505050", troughcolor='#696969')
-msg_list = tkinter.Listbox(messages_frame, height=30, width=100, yscrollcommand=scrollbar.set, bg = '#696969', fg = '#ffffff')
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
-messages_frame.pack()
+scroll = tkinter.Scrollbar(msgFrame, bg = "#505050", troughcolor='#696969')
+messagelist = tkinter.Listbox(msgFrame, height=30, width=100, yscrollcommand=scroll.set, bg = '#696969', fg = '#ffffff')
+scroll.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+messagelist.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+messagelist.pack()
+msgFrame.pack()
 
 # Create a frame for configuration (chat rooms and username)
-cfg_frame = tkinter.Frame(top, bg = '#505050', width = 25)
+cfg_frame = tkinter.Frame(wind, bg = '#505050', width = 25)
 
 # Initialize username entry field and its label, and place them in the window
-username_label = tkinter.Label(cfg_frame, text="Enter username: ", bg = '#505050', fg = '#ffffff')
+username_label = tkinter.Label(cfg_frame, text="Enter your username: ", bg = '#505050', fg = '#ffffff')
 username_label.pack(anchor='w', padx = 10)
-username_field = tkinter.Entry(cfg_frame, textvariable=my_username, bg = '#696969', fg = '#ffffff')
+username_field = tkinter.Entry(cfg_frame, textvariable=myUsername, bg = '#696969', fg = '#ffffff')
 username_field.pack(anchor='w', padx = 10, pady = 5)
 
 # Create a frame for the message box and send button
-send_frame = tkinter.Frame(top, bg = '#505050')
+send_frame = tkinter.Frame(wind, bg = '#505050')
 
 # Initialize message entry field, Send button, and field label, and place them in the window
-message_label = tkinter.Label(send_frame, text="Enter message: ", bg = '#505050', fg = '#ffffff')
-message_label.pack(anchor='nw', padx = 10)
-entry_field = tkinter.Entry(send_frame, textvariable=my_msg, width=50, bg = '#696969', fg = '#ffffff')
-entry_field.bind("<Return>", send)  # Add event listener for pressing enter in the message entry field, which acts the same as the send button
-entry_field.pack(anchor='nw', padx = 10, pady = 5)
-send_button = tkinter.Button(send_frame, text="Send", command=send, bg = '#696969', fg = '#ffffff')
-send_button.pack(anchor='nw', padx = 10, pady = 5)
+msgLabel = tkinter.Label(send_frame, text="Enter your message: ", bg = '#505050', fg = '#ffffff')
+msgLabel.pack(anchor='nw', padx = 10)
+entryField = tkinter.Entry(send_frame, textvariable=myMessage, width=50, bg = '#696969', fg = '#ffffff')
+entryField.bind("<Return>", sendMessage)  # Add event listener for pressing enter in the message entry field, which acts the same as the send button
+entryField.pack(anchor='nw', padx = 10, pady = 5)
+sndBtn = tkinter.Button(send_frame, text="Send", command=sendMessage, bg = '#696969', fg = '#ffffff')
+sndBtn.pack(anchor='nw', padx = 10, pady = 5)
 
 # Attach event listener for closing the window, which will send an exit command to the server
-top.protocol("WM_DELETE_WINDOW", on_closing)
+wind.protocol("WM_DELETE_WINDOW", closing)
 
 # Get arguments from command line
 if (len(sys.argv) < 3):
@@ -153,42 +156,42 @@ BUFFER_SIZE = 1024
 ADDR = (ip, port)
 
 # Create socket and connect to the server
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(ADDR)
+clntSocket = socket(AF_INET, SOCK_STREAM)
+clntSocket.connect(ADDR)
 
 # Get first message from server, which will be the number of chat rooms available
-first_msg = client_socket.recv(BUFFER_SIZE).decode("utf8")
-number_of_rooms = int(first_msg)
+firstMesssage = clntSocket.recv(BUFFER_SIZE).decode("utf8")
+numberOfRooms = int(firstMesssage)
 
 # Set up room selection menu
-chatRoomSelected = tkinter.StringVar(top)
-chatRoomSelected.set("List Of Chat Rooms")
+chatRoomSel = tkinter.StringVar(wind)
+chatRoomSel.set("List Of Chat Rooms")
 
 # Populate rooms menu based on maximum number of rooms
-rooms_list = []
-for i in range(number_of_rooms):
-    rooms_list.append("Chat Room " + str(i + 1))
-chat_rooms = tkinter.OptionMenu(cfg_frame, chatRoomSelected, *rooms_list)
-chat_rooms.config(bg = '#696969', fg = '#ffffff')
-chat_rooms.pack(anchor='w', padx = 10, pady = 5)
+listOfRooms = []
+for i in range(numberOfRooms):
+    listOfRooms.append("Chat Room " + str(i + 1))
+chatRooms = tkinter.OptionMenu(cfg_frame, chatRoomSel, *listOfRooms)
+chatRooms.config(bg = '#696969', fg = '#ffffff')
+chatRooms.pack(anchor='w', padx = 10, pady = 5)
 
 # Initialize change room button and attach event handler
-change_button = tkinter.Button(cfg_frame, text="Change Room", command=change_room, bg = '#696969', fg = '#ffffff')
-change_button.pack(anchor='w', padx = 10, pady = 5)
+chngBtn = tkinter.Button(cfg_frame, text="Change Room", command=changeRoom, bg = '#696969', fg = '#ffffff')
+chngBtn.pack(anchor='w', padx = 10, pady = 5)
 cfg_frame.pack(side = tkinter.LEFT)
 send_frame.pack(side = tkinter.RIGHT, expand = True)
 
 # Create thread for receiving data
-receive_thread = Thread(target=receive)
+rcvThread = Thread(target=receiveData)
 
 # Start receiving thread
-receive_thread.start()
+rcvThread.start()
 
 # Set window to non-resizable and launch GUI
-top.resizable(width=False, height=False)
+wind.resizable(width=False, height=False)
 
 # Initially join room 1, which will always exist
-chatRoomSelected.set("Chat Room 1")
-change_room()
+chatRoomSel.set("Chat Room 1")
+changeRoom()
 
 tkinter.mainloop()
